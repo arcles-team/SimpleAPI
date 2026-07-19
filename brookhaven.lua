80afb4434b88366ed89798fd83514d2e9ebc4eb9
Arcles <arcles@protonmail.com>
2026-07-19 22:20:06 +0200

    Update brookhaven.lua

--- brookhaven.lua
+++ brookhaven.lua
@@ -1,26 +1,41 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local NameColorEvent = ReplicatedStorage.RE["1RPNam1eColo1r"]
-local NameTextEvent  = ReplicatedStorage.RE["1RPNam1eTex1t"]
-local NotifyEvent    = ReplicatedStorage.Remotes.Notify
+local NameColorEvent = ReplicatedStorage.RE["RPNameColor"]
+local NameTextEvent = ReplicatedStorage.RE["RPNameText"]
+local NotifyEvent = ReplicatedStorage.Remotes.Notify
 
-local COLOR_RED  = Color3.new(1, 0, 0.06)
-local COLOR_BLUE = Color3.new(0, 0.21, 1)
+local API = {}
 
-local function loopColorChange()
-    while true do
-        NameColorEvent:FireServer("PickingRPNameColor", COLOR_RED)
-        task.wait(0.1)
-        
-        NameColorEvent:FireServer("PickingRPNameColor", COLOR_BLUE)
-        task.wait(0.1)
-    end
+function API.loopColorChange()
+    task.spawn(function()
+        while true do
+            NameColorEvent:FireServer(
+                "PickingRPNameColor",
+                Color3.new(1, 0, 0.06)
+            )
+
+            task.wait(0.1)
+
+            NameColorEvent:FireServer(
+                "PickingRPNameColor",
+                Color3.new(0, 0.21, 1)
+            )
+
+            task.wait(0.1)
+        end
+    end)
 end
 
-local function initthing(rpn, rpb, notif)
-  NameTextEvent:FireServer("RolePlayName", rpn)
-  NameTextEvent:FireServer("RolePlayBio", rpb)
-  firesignal(NotifyEvent.OnClientEvent, notif, nil, nil)
+function API.initthing(rpName, rpBio, notification)
+    NameTextEvent:FireServer("RolePlayName", rpName)
+    NameTextEvent:FireServer("RolePlayBio", rpBio)
+
+    firesignal(
+        NotifyEvent.OnClientEvent,
+        notification,
+        nil,
+        nil
+    )
 end
 
-loopColorChange()
+return API
